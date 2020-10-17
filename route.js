1ab14e6f939e82409fe8031fa6d367f0242b05a2
nicoWeb31 <nico.riot@free.fr>
2020-10-17 19:15:23 +0200

    separation route file

---
+++ route.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+
+
+
+const requestHandler = (req,res) =>{
+
+    const url = req.url
+    const methode =req.method
+
+    if(url === '/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>')
+        res.write('<head><title>toto</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">submit</button></form></body>')
+    
+        res.write('</html>')
+        return res.end();
+    }
+    
+    
+    if(url === '/message' && methode ==='POST'){
+        const body =[];
+        req.on('data',(chunk)=>{
+            console.log(chunk)
+            body.push(chunk)
+        })
+        return req.on('end',()=>{
+            const bodyPareser = Buffer.concat(body).toString();
+            console.log(bodyPareser)
+            const message = bodyPareser.split('=')[1]
+            fs.writeFile('datafromClient.txt',message,(err)=>{
+    
+                res.statusCode =302;
+                res.setHeader('Location','/')
+                return res.end();
+            })
+            
+        })
+    }
+    
+    
+    //console.log(req.url,req.method,req.headers)
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>toto</title></head>')
+    res.write('</html>')
+    res.end();
+}
+
